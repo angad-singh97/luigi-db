85eeb3caea81770babe3c278a87acc7744d2b6c9
MintGreenTZ <mintgreen0529@gmail.com>
2024-03-28 20:15:50 +0000

    [Fix] CE

--- src/deptran/mongodb/server.h
+++ src/deptran/mongodb/server.h
@@ -11,19 +11,18 @@ namespace janus {
 
 class MongodbServer : public TxLogServer {
   std::function<void(shared_ptr<Marshallable>)> callback_func_ = std::bind(&janus::TxLogServer::CommandEndCallback, this, std::placeholders::_1);
-  MongodbConnectionThreadPool mongodb_(100, callback_func_);
+  shared_ptr<MongodbConnectionThreadPool> mongodb_ = make_shared<MongodbConnectionThreadPool>(100, callback_func_);
  public:
-  void Setup() {
+  void Setup() override {
   }
   bool IsLeader() override {
     return loc_id_ == 0;
   }
   void Submit(const shared_ptr<Marshallable>& cmd) {
-    mongodb_.MongodbRequest(cmd);
+    mongodb_->MongodbRequest(cmd);
   }
   ~MongodbServer() {
-    mongodb_.Close();
+    mongodb_->Close();
   }
 };
-
 }
