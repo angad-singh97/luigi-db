8019b610bafb0241c0bfc11d50dc3ad3abce916c
MintGreenTZ <mintgreen0529@gmail.com>
2024-04-07 21:16:57 +0000

    [Revert] 37de88cd007635ff1ff72ee025eaa33365321c25 [Now] Mongodb have make sense throughput, but with high tail latency

--- src/deptran/mongodb/server.h
+++ src/deptran/mongodb/server.h
@@ -12,8 +12,24 @@ namespace janus {
 class MongodbServer : public TxLogServer {
   const int mongodb_connection_ = 100;
 
-  // std::function<void(shared_ptr<Marshallable>)> callback_func_ = std::bind(&janus::MongodbServer::CommandEndCallback, this, std::placeholders::_1);
-  shared_ptr<MongodbConnectionThreadPool> mongodb_ = make_shared<MongodbConnectionThreadPool>(mongodb_connection_);
+  // shared_ptr<MongodbConnectionThreadPool> mongodb_ = make_shared<MongodbConnectionThreadPool>(mongodb_connection_);
+
+  std::function<void(shared_ptr<Marshallable>)> callback_func_ = std::bind(&janus::MongodbServer::CommandEndCallback, this, std::placeholders::_1);
+  shared_ptr<MongodbConnectionThreadPool> mongodb_ = make_shared<MongodbConnectionThreadPool>(mongodb_connection_, callback_func_);
+  std::mutex ready_to_app_next_lock_;
+  std::queue<shared_ptr<Marshallable>> ready_to_app_next_;
+  void check_app_next() {
+    lock_guard<std::mutex> guard(ready_to_app_next_lock_);
+    while (ready_to_app_next_.size() > 0) {
+      app_next_(*ready_to_app_next_.front());
+      ready_to_app_next_.pop();
+    }
+  }
+  void CommandEndCallback(const shared_ptr<Marshallable>& cmd) {
+    RuleWitnessGC(cmd);
+    lock_guard<std::mutex> guard(ready_to_app_next_lock_);
+    ready_to_app_next_.push(cmd);
+  }
 
  public:
   void Setup() override {
@@ -22,11 +38,12 @@ class MongodbServer : public TxLogServer {
     return loc_id_ == 0;
   }
   void Submit(const shared_ptr<Marshallable>& cmd) {
-    // WAN_WAIT
+    WAN_WAIT
     mongodb_->MongodbRequest(cmd);
-    // WAN_WAIT
-    RuleWitnessGC(cmd);
-    app_next_(*cmd);
+    WAN_WAIT
+    check_app_next();
+    // RuleWitnessGC(cmd);
+    // app_next_(*cmd);
   }
   ~MongodbServer() {
     mongodb_->Close();
