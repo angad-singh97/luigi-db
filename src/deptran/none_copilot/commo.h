1e9ddeb3dd1762dc0925059f86c600817426f0c1
MintGreenTZ <mintgreen0529@gmail.com>
2024-04-09 07:10:31 +0000

    [Feature] Copilot can run at high latency high # clients setting [Feature] enable Copilot ping pang batching (disable will cause bug)

--- src/deptran/none_copilot/commo.h
+++ src/deptran/none_copilot/commo.h
@@ -8,7 +8,7 @@ namespace janus
 class CommunicatorNoneCopilot : public Communicator {
 public:
     uint32_t n_pending_rpc_[2] = {0,0};
-    const uint32_t max_pending_rpc_ = 200;
+    const uint32_t max_pending_rpc_ = 200 * 100; // [JetPack] * 100 for latancy case
     SharedIntEvent dispatch_quota{};
 
     CommunicatorNoneCopilot(PollMgr* poll_mgr = nullptr)
