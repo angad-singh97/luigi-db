45cd70555a58f25082c9878914e182a4e199a36a
MintGreenTZ <mintgreen0529@gmail.com>
2024-04-13 00:42:46 +0000

    [Feature] mongocxx (mongodb client) installation

--- dep.sh
+++ dep.sh
@@ -24,5 +24,21 @@ sudo apt-get --assume-yes install -y \
 sudo wget https://github.com/mikefarah/yq/releases/download/v4.24.2/yq_linux_amd64 \
     -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
 
+pip install -U setuptools
 pip3 install -r requirements.txt
 pip3 install Pillow matplotlib pyyaml
+
+# mongodb client
+cd ~
+curl -OL https://github.com/mongodb/mongo-cxx-driver/releases/download/r3.10.1/mongo-cxx-driver-r3.10.1.tar.gz
+tar -xzf mongo-cxx-driver-r3.10.1.tar.gz
+cd mongo-cxx-driver-r3.10.1/build
+cmake ..                                \
+    -DCMAKE_BUILD_TYPE=Release          \
+    -DMONGOCXX_OVERRIDE_DEFAULT_INSTALL_PREFIX=OFF
+cmake ..                                            \
+    -DCMAKE_BUILD_TYPE=Release                      \
+    -DBSONCXX_POLY_USE_BOOST=1                      \
+    -DMONGOCXX_OVERRIDE_DEFAULT_INSTALL_PREFIX=OFF
+cmake --build .
+sudo cmake --build . --target install
