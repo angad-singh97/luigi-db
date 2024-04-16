539d410a32cb04d371bb240758480b50cb1220b4
MintGreenTZ <mintgreen0529@gmail.com>
2024-04-16 03:21:39 +0000

    [Fix] close file after dump

--- src/deptran/s_main.cc
+++ src/deptran/s_main.cc
@@ -464,6 +464,8 @@ int main(int argc, char *argv[]) {
         }
     }
     Log_info("Dumped to %s with %d lines data", dump_file_name.c_str(), max_size);
+    file.close();
+    Log_info("%s closed", dump_file_name.c_str());
   }
 #ifdef LATENCY_DEBUG
   Log_info("client2leader 50pct %.2f 90pct %.2f 99pct %.2f", client2leader.pct50(), client2leader.pct90(), client2leader.pct99());
