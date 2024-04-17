8949e09d476a3673e2cdeff12b4d2ac5925f54b0
MintGreenTZ <mintgreen0529@gmail.com>
2024-04-17 20:13:28 +0000

    [Add] mongodb community in requirements

--- dep.sh
+++ dep.sh
@@ -48,3 +48,14 @@ cmake ..                                            \
     -DMONGOCXX_OVERRIDE_DEFAULT_INSTALL_PREFIX=OFF
 cmake --build .
 sudo cmake --build . --target install
+
+# Install MongoDB Community Edition on Ubuntu : https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+
+sudo apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
