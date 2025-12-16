df60f6ec902336dcf9ff5b2fdf75dcf9af3f7fbf
Avjot <avjot.singh99@gmail.com>
2025-12-16 18:40:35 +0000

    Simplify luigi_server_main.cc - move all setup into server->Run()

--- src/deptran/luigi/luigi_server_main.cc
+++ src/deptran/luigi/luigi_server_main.cc
@@ -12,16 +12,12 @@
  */
 
 #include <iostream>
-#include <memory>
+#include <string>
 #include <thread>
-#include <vector>
 
 #include "benchmarks/benchmark_config.h"
-#include "benchmarks/mbta_wrapper.hh"
 #include "benchmarks/rpc_setup.h"
-#include "deptran/luigi/luigi_owd.h"
 #include "deptran/luigi/luigi_server.h"
-#include "deptran/luigi/luigi_state_machine.h"
 #include "lib/common.h"
 
 using namespace std;
@@ -52,11 +48,12 @@ int main(int argc, char **argv) {
   // Parse command line arguments (reuse Mako's pattern)
   string site_name;
   string benchmark_type = "tpcc";
+  string config_file;
 
   for (int i = 1; i < argc; i++) {
     string arg = argv[i];
     if (arg == "--shard-config" && i + 1 < argc) {
-      cfg.setConfigFile(argv[++i]);
+      config_file = argv[++i];
     } else if (arg == "--site-name" && i + 1 < argc) {
       site_name = argv[++i];
     } else if (arg == "--num-threads" && i + 1 < argc) {
@@ -72,14 +69,14 @@ int main(int argc, char **argv) {
   }
 
   // Validate required arguments
-  if (cfg.getConfigFile().empty() || site_name.empty()) {
+  if (config_file.empty() || site_name.empty()) {
     cerr << "Error: --shard-config and --site-name are required\n";
     print_usage(argv[0]);
     return 1;
   }
 
   // Load configuration (set global variable)
-  config = new transport::Configuration(cfg.getConfigFile());
+  config = new transport::Configuration(config_file);
   cfg.setConfig(config);
 
   // Get site info and set shard index
@@ -113,94 +110,28 @@ int main(int argc, char **argv) {
        << "Threads:    " << cfg.getNthreads() << "\n"
        << "===================================\n\n";
 
-  // Initialize Luigi OWD service
-  cout << "Initializing Luigi OWD service...\n";
-  auto &owd = mako::luigi::LuigiOWD::getInstance();
-  owd.init(cfg.getConfigFile(), cfg.getCluster(), cfg.getShardIndex(),
-           cfg.getNshards());
-  owd.start();
-
-  // Create database instance (following simpleTransaction.cc pattern)
-  cout << "Creating database...\n";
-  abstract_db *db = new mbta_wrapper;
-  map<int, abstract_ordered_index *> tables;
-
-  // TODO: Open tables based on benchmark type
-  // For TPC-C: customer, district, warehouse, etc.
-  // For Micro: simple key-value table
-
   // Create LuigiServer instance
+  // All setup (OWD, scheduler, state machine, RPC) happens inside server->Run()
   cout << "Creating Luigi server...\n";
   int partition_id = 0; // For single-partition mode
-  LuigiServer *server = new LuigiServer(cfg.getConfigFile(),
+  LuigiServer *server = new LuigiServer(config_file,
                                         cfg.getShardIndex(), // client_shard_idx
                                         cfg.getShardIndex(), // server_shard_idx
                                         partition_id);
 
-  // Create helper queues (reuse Mako's pattern)
-  mako::HelperQueue *queue = new mako::HelperQueue();
-  mako::HelperQueue *queue_response = new mako::HelperQueue();
+  // TODO: Pass benchmark_type to server so it knows which state machine to
+  // create For now, server will need to be configured or default to TPC-C
 
-  // Register database and tables
-  if (db && !tables.empty()) {
-    server->Register(db, queue, queue_response, tables);
-  }
-
-  // Setup RPC connections to other shards
-  cout << "Setting up RPC connections...\n";
-  // TODO: Create RPC server and poll thread
-  // TODO: Get shard addresses from config
-  // TODO: Call server->SetupRpc(rpc_server, poll_thread, shard_addresses)
-
-  // Create state machine based on benchmark type
-  cout << "Creating state machine (" << benchmark_type << ")..\n";
-  shared_ptr<LuigiStateMachine> state_machine;
-
-  if (benchmark_type == "tpcc") {
-    state_machine =
-        make_shared<LuigiTPCCStateMachine>(cfg.getShardIndex(), // shard_id
-                                           0,                   // replica_id
-                                           cfg.getNshards(),    // shard_num
-                                           1                    // replica_num
-        );
-  } else if (benchmark_type == "micro") {
-    state_machine = make_shared<LuigiMicroStateMachine>(cfg.getShardIndex(), 0,
-                                                        cfg.getNshards(), 1);
-  } else {
-    cerr << "Error: Unknown benchmark type '" << benchmark_type << "'\n";
-    delete server;
-    delete queue;
-    delete queue_response;
-    owd.stop();
-    return 1;
-  }
-
-  // Initialize state machine tables
-  state_machine->InitializeTables();
-
-  // Get scheduler from server and set state machine
-  auto *scheduler = server->GetScheduler();
-  if (scheduler) {
-    scheduler->SetStateMachine(state_machine);
-    scheduler->EnableStateMachineMode(true);
-    scheduler->SetWorkerCount(cfg.getNthreads());
-  }
-
-  cout << "\n=== Luigi Server Ready ===\n";
-  cout << "Listening for requests...\n";
+  cout << "\n=== Starting Luigi Server ===\n";
+  cout << "Server will initialize OWD, scheduler, and state machine...\n";
   cout << "Press Ctrl+C to stop\n\n";
 
-  // Run server event loop
+  // Run server event loop (this does all the setup internally)
   server->Run();
 
   // Cleanup
   cout << "\nShutting down...\n";
   delete server;
-  delete queue;
-  delete queue_response;
-  if (db)
-    delete db;
-  owd.stop();
 
   cout << "Luigi server stopped\n";
   return 0;
